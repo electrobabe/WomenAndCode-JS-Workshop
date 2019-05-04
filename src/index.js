ad691e7dea2081569473dc59fe08426606e4018f
Barbara Ondrisek <office@electrobabe.at>
2019-05-04 14:23:58 +0200

    add event listener to button

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@ document.getElementById("app").innerHTML = `
 <input type="text" class="secondInput">
 <button>click me</button
 `;
+
+// alert("lalala");
+
+const apples = 5;
+const pears = 2;
+
+const fruit = apples + pears;
+
+console.log("fruit: " + fruit);
+
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+
+button.addEventListener("click", function() {
+  console.log("meep!");
+  body.classList.toggle("colorChange");
+});
